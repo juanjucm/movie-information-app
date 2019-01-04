f2bdb60a87d4b08282619148a78b4f3918215a4b
Juan Julián <juanju@juans-mac.local>
2019-01-05 00:03:21 +0100

    New style added (fix minor bugs)

--- Moviepedia/Controllers/FilmsViewController.swift
+++ Moviepedia/Controllers/FilmsViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class FilmsTableViewCell: UITableViewCell{
 
     @IBOutlet weak var filmImage: UIImageView!
+    @IBOutlet weak var filmImageFrame: UIView!
     @IBOutlet weak var filmYearLabel: UILabel!
     @IBOutlet weak var filmDirectorLabel: UILabel!
     @IBOutlet weak var filmNameLabel: UILabel!
@@ -32,8 +33,21 @@ class FilmsViewController: UITableViewController {
         cell.filmNameLabel?.text = film.name
         cell.filmDirectorLabel?.text = film.director.name
         cell.filmYearLabel.text = film.year
+        
+        cell.filmImageFrame.clipsToBounds = false
+        cell.filmImageFrame.layer.shadowColor = UIColor.black.cgColor
+        cell.filmImageFrame.layer.shadowOpacity = 1
+        cell.filmImageFrame.layer.shadowOffset = CGSize.zero
+        cell.filmImageFrame.layer.shadowRadius = 10
+        cell.filmImageFrame.layer.shadowPath = UIBezierPath(roundedRect: cell.filmImageFrame.bounds, cornerRadius: 10).cgPath
+        
         cell.filmImage?.image = UIImage(named: film.photo)
+        cell.filmImage?.layer.cornerRadius = 10.0
+        cell.filmImage?.clipsToBounds = true
+        cell.filmImage.frame = cell.filmImageFrame.bounds
+        
+        cell.filmImageFrame.addSubview(cell.filmImage)
+        
         return cell
     }
 }
-
